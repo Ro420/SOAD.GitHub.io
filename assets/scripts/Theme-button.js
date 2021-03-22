f80440e2c1b9815f08caa91c9f5bfa3171284a1f
Ro420 <wpff.rly7@yahoo.com>
2021-03-22 22:01:52 +0200

    First launch of Ju420

---
+++ assets/scripts/Theme-button.js
@@ -0,0 +1,100 @@
+const whiteThemeClass = 'whiteTheme';
+const blackThemeClass = 'blackTheme';
+
+const blackThemeButtonId = 'blackThemeButton';
+const whiteThemeButtonId = 'whiteThemeButton';
+
+const blackThemeButton = document.getElementById(blackThemeButtonId);
+const whiteThemeButton = document.getElementById(whiteThemeButtonId);
+
+const whiteFooterClass = 'whiteFooter';
+const blackFooterClass = 'blackFooter';
+
+const whiteFooterId = 'whiteThemeFooter';
+const blackFooterId = 'blackThemeFooter';
+
+const whiteThemeFooter = document.getElementById(whiteFooterId)
+const blackThemeFooter = document.getElementById(blackFooterId)
+
+blackThemeButton.addEventListener('mouseenter', mouseEnterBlack);
+blackThemeButton.addEventListener('mouseout', mouseOutBlack);
+blackThemeButton.addEventListener('click', onClickBlack);
+
+whiteThemeButton.addEventListener('mouseenter', mouseEnterWhite);
+whiteThemeButton.addEventListener('mouseout', mouseOutWhite);
+whiteThemeButton.addEventListener('click', onClickWhite);
+
+whiteThemeFooter.addEventListener('mouseenter', mouseEnterBlack);
+whiteThemeFooter.addEventListener('mouseout', mouseOutBlack);
+whiteThemeFooter.addEventListener('click', onClickBlack);
+
+blackThemeFooter.addEventListener('mouseenter', mouseEnterWhite);
+blackThemeFooter.addEventListener('mouseout', mouseOutWhite);
+blackThemeFooter.addEventListener('click', onClickWhite);
+
+const whiteTheme = 'white';
+const blackTheme = 'black';
+const darkBlueTheme = 'darkblue';
+
+let activeTheme = whiteTheme;
+let activeFooter = blackFooter;
+
+const activeClassName = 'active'
+
+function mouseEnterBlack() {
+  if (activeTheme === whiteTheme) {
+    document.body.classList.remove(whiteThemeClass);
+    document.body.classList.add(blackThemeClass);
+    document.footer.classList.remove(blackFooterClass);
+    document.footer.classList.add(whiteFooterClass);
+  }
+}
+
+function mouseOutBlack() {
+  if (activeTheme === whiteTheme) {
+    document.body.classList.remove(blackThemeClass);
+    document.body.classList.add(whiteThemeClass);
+    document.footer.classList.remove(whiteFooterClass);
+    document.footer.classList.add(blackFooterClass);
+  }
+}
+
+function onClickBlack() {
+  if (activeTheme === whiteTheme) {
+    activeTheme = blackTheme;
+    blackThemeButton.classList.add(activeClassName);
+    whiteThemeFooter.classList.add(activeClassName);
+  } else {
+    whiteThemeButton.classList.remove(activeClassName);
+    blackThemeFooter.classList.remove(activeClassName);
+  }
+}
+
+function mouseEnterWhite() {
+  if (activeTheme === blackTheme) {
+    document.body.classList.remove(blackThemeClass);
+    document.body.classList.add(whiteThemeClass);
+    document.footer.classList.remove(whiteFooterClass);
+    document.footer.classList.add(blackFooterClass);
+  }
+}
+
+function mouseOutWhite() {
+  if (activeTheme === blackTheme) {
+    document.body.classList.remove(whiteThemeClass);
+    document.body.classList.add(blackThemeClass);
+    document.footer.classList.remove(blackFooterClass);
+    document.footer.classList.add(whiteFooterClass);
+  }
+}
+
+function onClickWhite() {
+  if (activeTheme === blackTheme) {
+    activeTheme = whiteTheme;
+    whiteThemeButton.classList.add(activeClassName);
+    blackThemeFooter.classList.add(activeClassName);
+  } else {
+    blackThemeButton.classList.remove(activeClassName); 
+    whiteThemeFooter.classList.remove(activeClassName);
+  }
+}
